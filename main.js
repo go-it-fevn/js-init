88fc233031723e9f2506fcbcf520f8d314684b8c
Andrii Shtadler <andrii_shtadler@epam.com>
2020-01-13 21:41:11 +0200

    init

---
+++ main.js
@@ -0,0 +1,125 @@
+'use strict'
+// // prim
+// const string = 'my cool string 🤟';
+// const number = 777;
+// const number2 = NaN;
+// const number3 = Infinity;
+// const bool = true;
+// const bool2 = false;
+// const specOne = undefined;
+// const specTwo = null;
+
+// const myObj = {}
+
+// console.log(myObj)
+
+// let first = 'andrii';
+// let second = first;
+// console.log(second);
+
+// second = 'Vasya';
+// console.log(second, first);
+
+// const user = {
+//   name: 'Andrii',
+//   surname: 'Shtadler'
+// }
+
+// console.log(user.name)
+
+// const student = user;
+// student.name = 'Vasya'
+
+// console.log(user)
+
+// const a = {
+//   name: 'Andrii'
+// }
+// const b = {
+//   name: 'Andrii'
+// }
+
+// console.log(a === b)
+
+// const array = ['hello', 'my', 'friend']
+// const userInput = prompt()
+// const expected = 'js-1';
+// const expected2 = 'js-2';
+
+// if(userInput === expected && userInput === expected2) {
+//   console.log('Removed')
+// } else {
+//   console.log('NЭT')
+// }
+
+// userInput == 'js-1' ? console.log('123123123') : console.log(false)
+
+// if(userInput=='js-1') {
+//   console.log('123123123')
+// } else {
+//   console.log(false)
+// }
+
+// const discount = userInput === 'friday' ? 30 : 20
+// console.log(discount)
+
+// const name = 'Andrii'
+// const surname = 'Shtadler'
+// const string = 'My name is'
+// const postMessage = 'and surname is'
+
+// // console.log(string+name+postMessage+surname)
+// console.log(`Hello ${string} ${name} ${postMessage} ${surname} space space space`)
+
+// const template = 
+// `
+// <div>
+//   <h1>Hello ${name} </h1>
+// </div>
+// `
+
+// let something;
+// console.log(something)
+// something = 2;
+// console.log(something)
+
+console.log(something)
+var something = 2;
+
+// switch(userInput) {
+//   case 'js-1': {
+//     let a = 2;
+//     console.log('Hi')
+//     console.log('js-1???')
+//     break;
+//   }
+//   case 'js-3':
+//   case 'js-2':
+//     console.log('js-2 or js-3')
+//     break;
+
+//   default:
+//     console.log('any')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
